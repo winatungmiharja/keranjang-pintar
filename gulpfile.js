e97833fa815297a36c7362b0687912f7920caba3
Wina Tungmiharja <64743796+winatungmiharja@users.noreply.github.com>
2021-06-27 02:51:00 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const gulp = require("gulp");
-const gap = require("gulp-append-prepend");
-
-gulp.task("licenses", async function () {
-  // this is to add Creative Tim licenses in the production mode for the minified js
-  gulp
+gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
-      gap.prependText()
+      gap.prependText(`/*!
+=========================================================
+* Paper Dashboard React - v1.3.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2021 Creative Tim (http://www.creative-tim.com)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
@@ -14,7 +18,16 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/index.html", { base: "./" })
     .pipe(
-      gap.prependText()
+      gap.prependText(`<!--
+=========================================================
+* Paper Dashboard React - v1.3.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2021 Creative Tim (http://www.creative-tim.com)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
@@ -22,8 +35,16 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
     .pipe(
-      gap.prependText()
+      gap.prependText(`/*!
+=========================================================
+* Paper Dashboard React - v1.3.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2021 Creative Tim (http://www.creative-tim.com)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
-});
